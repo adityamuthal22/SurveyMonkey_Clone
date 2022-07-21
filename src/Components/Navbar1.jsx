c1b7e11151cda1bae3a72486e88a92ef3b886397
adityamuthal22 <adityamuthal.2208@gmail.com>
2022-07-22 03:13:54 +0530

    All day-3 work Added

--- src/Components/Navbar1.jsx
+++ src/Components/Navbar1.jsx
@@ -18,7 +18,9 @@ const Navbar1 = () => {
         position:"fixed",
         top:"0",
        backgroundColor:"white",
-       alignItems:"center"
+       alignItems:"center",
+      zIndex:3,
+         boxShadow: "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px",
         }}>
         <Box width="200px" >
            <Link to="/">
